4d4cd07bb4458329f9837615cb5b76e4e000a4db
DiegoJesuP <spartfomi@outlook.com>
2025-02-27 21:59:23 -0600

    get - post - put - delete con front

--- Proyecto-B/errorPages/alumnos/views.py
+++ Proyecto-B/errorPages/alumnos/views.py
@@ -3,6 +3,10 @@ from rest_framework import viewsets
 from .models import Alumno
 from .serializers import AlumnoSerializer
 
+from django.shortcuts import render, redirect
+from .forms import AlumnoForm
+
+
 class AlumnoViewSet(viewsets.ModelViewSet):
     # esta variable me dice de donde saco el modelo y la informacion de DB
     queryset = Alumno.objects.all()
@@ -16,3 +20,7 @@ class AlumnoViewSet(viewsets.ModelViewSet):
     #permitir filtrar que metodos HTTP se pueden usar
     #por defecto si no la delcaro se ocupan todos (get, post, put, delete)
     #http_method_names = ['get', 'post', 'put', 'delete']
+    
+def alumnos_view(request):
+    form = AlumnoForm()
+    return render(request, 'hernandez_diego.html', {'form': form})
