f973ce691885194133be7e7f0c507d2f8593275e
DiegoJesuP <spartfomi@outlook.com>
2025-03-05 23:45:40 -0600

    aniamciones y relaciones

--- Proyecto-B/errorPages/productos/views.py
+++ Proyecto-B/errorPages/productos/views.py
@@ -3,6 +3,9 @@ from rest_framework import viewsets
 from .models import Producto
 from .serializers import ProductoSerializer
 
+from django.shortcuts import render, redirect
+from .forms import ProductoForm
+
 class ProductoViewSet(viewsets.ModelViewSet):
     # esta variable me dice de donde saco el modelo y la informacion de DB
     queryset = Producto.objects.all()
@@ -16,3 +19,7 @@ class ProductoViewSet(viewsets.ModelViewSet):
     #permitir filtrar que metodos HTTP se pueden usar
     #por defecto si no la delcaro se ocupan todos (get, post, put, delete)
     #http_method_names = ['get', 'post', 'put', 'delete']
+    
+def agregar_view(request):
+    form = ProductoForm()
+    return render(request, 'agregar.html', {'form': form})
